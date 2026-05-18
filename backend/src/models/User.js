2b53778a1f38a1d3597072bd2e793b68ef6a8762
ValemiMilk <nicolasimoes@valemilk.com.br>
2026-05-18 15:38:10 -0300

    fix: mongoose pre-save hook async sem next()

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -10,10 +10,9 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Hash da senha antes de salvar
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 12);
-  next();
 });
 
 userSchema.methods.verificarSenha = function (senha) {
